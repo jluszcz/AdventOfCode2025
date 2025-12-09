268f03c3d71d8d2a33fb6a28c6c15dfcc3ccc2fa
Jacob Luszcz <jacob.luszcz@gmail.com>
2025-12-09 09:56:15 -0500

    Update AdventOfCode-rs

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,64 +1,67 @@
 use anyhow::Result;
+use aoc_util::grid::{Grid, Point};
 use log::{debug, trace};
-use std::collections::HashSet;
 use std::fmt::Debug;
 
 #[derive(Debug)]
-struct Grid(Vec<Vec<bool>>);
+struct Floor(Grid<bool>);
 
-impl TryFrom<Vec<String>> for Grid {
+impl TryFrom<Vec<String>> for Floor {
     type Error = anyhow::Error;
 
     fn try_from(value: Vec<String>) -> Result<Self, Self::Error> {
         let mut grid = Vec::new();
 
-        for (y, row) in value.iter().enumerate() {
-            grid.push(vec![false; row.len()]);
-
-            for (x, col) in row.chars().enumerate() {
-                grid[y][x] = col == '@';
+        for row in value.iter() {
+            let mut grid_row = Vec::new();
+            for col in row.chars() {
+                grid_row.push(col == '@');
             }
+            grid.push(grid_row);
         }
 
-        Ok(Self(grid))
+        Ok(Self(Grid::try_from(grid)?))
     }
 }
 
-impl Grid {
-    fn is_roll_accessible(&self, x: usize, y: usize) -> bool {
-        if !self.0[y][x] {
-            trace!("No roll at {x}, {y}");
+impl Floor {
+    fn is_roll_accessible(&self, position: Point) -> bool {
+        if !self.0[position] {
+            trace!("No roll at {position:?}");
             return false;
         }
 
-        let adj_count = aoc_util::grid::neighbors(&self.0, x, y, true)
+        let adj_count = aoc_util::grid::neighbors(&self.0, position, true)
             .into_iter()
             .filter(|n| {
-                let neighbor_is_roll = self.0[n.position.1][n.position.0];
-                trace!("Neighbor at {x}, {y} is roll: {neighbor_is_roll}");
+                let neighbor_is_roll = self.0[n.position];
+                trace!("Neighbor at {position:?} is roll: {neighbor_is_roll}");
                 neighbor_is_roll
             })
             .count();
 
-        debug!("{x}, {y} has {adj_count} adjacent roll(s)");
+        debug!("{position:?} has {adj_count} adjacent roll(s)");
         adj_count < 4
     }
 
     fn remove_accessible_rolls(&mut self) -> usize {
         let mut count = 0;
 
-        let mut to_remove = HashSet::new();
+        let mut to_remove = Vec::new();
 
-        for x in 0..self.0.len() {
-            for y in 0..self.0[x].len() {
-                if self.is_roll_accessible(x, y) {
-                    to_remove.insert((x, y));
+        for y in 0..self.0.height() {
+            for x in 0..self.0.width() {
+                let position = Point::new(x, y);
+                if self.is_roll_accessible(position) {
+                    to_remove.push(position);
                     count += 1;
                 }
             }
         }
 
-        to_remove.iter().for_each(|&(x, y)| self.0[y][x] = false);
+        to_remove
+            .iter()
+            .for_each(|&position| self.0[position] = false);
 
         count
     }
@@ -80,7 +83,7 @@ impl Grid {
 }
 
 fn main() -> Result<()> {
-    let mut grid = Grid::try_from(aoc_util::init()?)?;
+    let mut grid = Floor::try_from(aoc_util::init()?)?;
 
     let accessible_roll_count = grid.accessible_roll_count();
     println!("{accessible_roll_count}");
@@ -94,7 +97,7 @@ mod tests {
 
     #[test]
     fn example() -> Result<()> {
-        let mut grid = Grid::try_from(aoc_util::init_test()?)?;
+        let mut grid = Floor::try_from(aoc_util::init_test()?)?;
 
         assert_eq!(43, grid.accessible_roll_count());
 
